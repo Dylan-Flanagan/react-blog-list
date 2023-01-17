a42287e161dfeac330f1437d8ed48bb59994af70
Dylan <dylflanagan@gmail.com>
2023-01-17 15:23:34 -0800

    Blogs rendering, authors table name column appearring, all tests passing

--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,6 +1,12 @@
 import { checkError, client } from './client.js';
 
+// export async function fetchBlogs() {
+//   const resp = await client.from('blogs').select('*');
+//   return checkError(resp);
+// }
+
 export async function fetchBlogs() {
-  const resp = await client.from('blogs').select('*');
+  const resp = await client.from('blogs').select('*, authors(name)');
+  //   console.log('resp', resp);
   return checkError(resp);
 }
